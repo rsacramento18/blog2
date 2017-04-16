ed69e67aca4e1d312b7c7a66266070c861c02e40
rsacramento18 <ricardo_sacramento@outlook.com>
2017-04-16 11:32:48 +0100

    Blog Design inicial

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+<meta charset="utf-8" />
+<title>Blog</title>
+ 
+<link href="https://fonts.googleapis.com/css?family=Anaheim|Cutive+Mono" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="css/main.css" type="text/css" />
+ 
+<!--[if IE]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+<!--[if lte IE 7]>
+  <script src="js/IE8.js" type="text/javascript"></script><![endif]-->
+<!--[if lt IE 7]>
+ 
+  <link rel="stylesheet" type="text/css" media="all" href="css/ie6.css"/><![endif]-->
+</head>
+ 
+<body id="index" class="home">
+
+    <div class="container-fluid effect8">
+        <div class="row">
+            <div id="logo"  class="col-lg-5 col-lg-offset-4">
+                <div id="text-logo" class="text-center">
+                    <h1>de A a Z...</h1>
+                </div>
+                
+                <div class="col-lg-12">
+                    <div id="nav">
+                        <ul class="list-inline">
+                            <li><a href="#aNossaHistoria">Blog</a></li>
+                            <li><a href="#destination" id="nav2">B</a></li>
+                            <li><a href="#servicos">Sobre nos</a></li>
+                            <li><a href="#" id="nav3">Contactos</a></li>
+                          </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    <div class="row">
+        <div class="col-lg-6">
+            <h1 class="postTittle">Titulo Post</h1>
+            <h2 class="postSubtittle">Subtitulo Post</h2>
+            <p>O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500, quando uma misturou os caracteres de um texto para criar um espécime de livro. Este texto não só sobreviveu 5 séculos, mas também o salto para a tipografia electrónica, mantendo-se essencialmente inalterada. Foi popularizada nos anos 60 com a disponibilização das folhas de Letraset, que continham passagens com Lorem Ipsum, e mais recentemente com os programas de publicação como o Aldus PageMaker que incluem versões do Lorem Ipsum.</p>
+
+
+        </div>
+    </div>
+
+    
+</body>
+</html>
