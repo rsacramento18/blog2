a089c6e2a9cd2115a389fb8fb0c196f97097eb5c
rsacramento18 <ricardo_sacramento@outlook.com>
2017-04-17 12:41:54 +0100

    Alteracoes na estrutura do blog

---
+++ Site/escrever.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+<meta charset="utf-8" />
+<title>Blog</title>
+ 
+<link href="https://fonts.googleapis.com/css?family=Anaheim|Cutive+Mono" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="css/main.css" type="text/css" />
+ 
+<!--[if IE]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+<!--[if lte IE 7]>
+  <script src="js/IE8.js" type="text/javascript"></script><![endif]-->
+<!--[if lt IE 7]>
+ 
+  <link rel="stylesheet" type="text/css" media="all" href="css/ie6.css"/><![endif]-->
+</head>
+ 
+<body id="index" class="home">
+
+    <?php
+    
+        
+
+    ?>
+
+    <div class="container-fluid effect8">
+        <div class="row">
+            <div id="logo"  class="col-lg-5 col-lg-offset-4">
+                <div id="text-logo" class="text-center">
+                    <h1>de A a Z...</h1>
+                </div>
+                
+                <div class="col-lg-5 col-lg-offset-3">
+                    <div id="nav">
+                        <ul class="list-inline">
+                            <li><a href="#">Escrever</a></li>
+                            <li><a href="#">Os meus Posts</a></li>
+                            <li><a href="#">Ficha de Escritor</a></li>
+                          </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+
+
+        <div class="row">
+            <div class="col-lg-2 effect8" id="sideMenu">
+                <h2>Por Mes</h2>
+                <ul>
+                    <li><a href="">Marco</a></li>
+                    <li><a href="">Fevereiro</a></li>
+                    <li><a href="">Janeiro</a></li>
+                </ul>
+                <h2>Por Autor</h2>
+                 <ul>
+                    <li><a href="">Mr.M</a></li>
+                    <li><a href="">Mr.F</a></li>
+                    <li><a href="">Mr.X</a></li>
+                </ul>
+                <h2>Escrever</h2>
+                <form action="escrever.php" name="formEscrever" id="formEscrever">
+                    <input type="text" size="20" name="user" id="user" placeholder="User"/></br>
+                    <input type="password" size="20" name="password" id="password" placeholder="Password"/></br>
+                    <input type="submit" value="Entrar" name="submitEscrever" id="subtmitEscrever"/>
+            </div>
+            <div class="col-lg-10  effect8 post">
+                <h1 class="postTittle">Titulo Post</h1>
+                <h2 class="postSubtittle">Subtitulo Post</h2>
+                <p>O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500, quando uma misturou os caracteres de um texto para criar um espécime de livro. Este texto não só sobreviveu 5 séculos, mas também o salto para a tipografia electrónica, mantendo-se essencialmente inalterada. Foi popularizada nos anos 60 com a disponibilização das folhas de Letraset, que continham passagens com Lorem Ipsum, e mais recentemente com os programas de publicação como o Aldus PageMaker que incluem versões do Lorem Ipsum.</p>
+                <p class="assinatura">Publicado a 12/04/2017 por Mr.M</p>
+            </div>
+
+            <div class="col-lg-10  effect8 post">
+                <h1 class="postTittle">Titulo Post</h1>
+                <h2 class="postSubtittle">Subtitulo Post</h2>
+                <p>O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500, quando uma misturou os caracteres de um texto para criar um espécime de livro. Este texto não só sobreviveu 5 séculos, mas também o salto para a tipografia electrónica, mantendo-se essencialmente inalterada. Foi popularizada nos anos 60 com a disponibilização das folhas de Letraset, que continham passagens com Lorem Ipsum, e mais recentemente com os programas de publicação como o Aldus PageMaker que incluem versões do Lorem Ipsum.</p>
+                <p class="assinatura">Publicado a 12/04/2017 por Mr.M</p>
+
+            </div>
+        </div>
+    </div>
+
+    
+</body>
+</html>
---
+++ mysqlconfig.php
@@ -0,0 +1,8 @@
+<?php
+/***** EDIT BELOW LINES *****/
+$DB_Server = "localhost"; // MySQL Server
+$DB_Username = "root"; // MySQL Username
+$DB_Password = "Agaporni1991"; // MySQL Password
+$DB_DBName = "blog"; // MySQL Database Name
+
+?>
